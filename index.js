ab06051dc24454e7a70117ef2a21a9612a1fd85c
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2022-01-27 16:29:22 -0300

    feat: Cria a rota /products e adiciona o errorMiddleware a aplicaçao

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const productsController = require('./controllers/productsController');
+
+const errorMiddleware = require('./middlewares/error');
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsController);
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
