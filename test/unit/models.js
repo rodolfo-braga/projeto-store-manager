f280280c4746025437e064d25cf0ca3c8701753b
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-02-07 22:35:25 -0300

    test(models): Adiciona teste unitário das funções getSales e getSaleById

--- test/unit/models.js
+++ test/unit/models.js
@@ -13,6 +13,23 @@ const saleMock = [
   }
 ];
 
+const saleFromDatabaseMock = [
+  {
+    "saleId": 1,
+    "date": "2022-02-07T03:00:00.000Z",
+    "product_id": 1,
+    "quantity": 10
+  }
+];
+
+const saleByIdMock = [
+  {
+    "date": "2022-02-07T03:00:00.000Z",
+    "product_id": 1,
+    "quantity": 10
+  }
+];
+
 describe('Testando a manipulação de produtos (models/productsModel)', () => {
   describe('Ao cadastrar um produto (productsModel.createProduct)', () => {
     before(() => {
@@ -85,7 +102,7 @@ describe('Testando a manipulação de produtos (models/productsModel)', () => {
         expect(product[0]).to.have.all.keys(["id", "name", "quantity"]);
       });
     });
-  })
+  });
 
   describe('Ao buscar todos os produtos (productsModel.getProducts)', () => {
     describe('Se não existir nenhum produto cadastrado', () => {
@@ -187,6 +204,118 @@ describe('Testando a manipulação de vendas (models/salesModel)', () => {
       const result = await salesModel.registerSale(saleMock);
       const [item] = result.itemsSold;
       expect(item).to.have.all.keys(["product_id", "quantity"]);
+    });
+  });
+
+  describe('Ao buscar todas as vendas (salesModel.getSales)', () => {
+    describe('Se não existir nenhuma venda cadastrada', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([[]]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('retorna um array', async () => {
+        const sales = await salesModel.getSales();
+        expect(sales).to.be.an('array');
+      });
+
+      it('o array está vazio', async () => {
+        const sales = await salesModel.getSales();
+        expect(sales).to.be.empty;
+      });
+    });
+
+    describe('Se existir pelo menos uma venda cadastrada', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([saleFromDatabaseMock]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('retorna um array', async () => {
+        const sales = await salesModel.getSales();
+        expect(sales).to.be.an('array');
+      });
+
+      it('o array não está vazio', async () => {
+        const sales = await salesModel.getSales();
+        expect(sales).not.to.be.empty;
+      });
+
+      it('todos os itens do array são do tipo "objeto"', async () => {
+        const sales = await salesModel.getSales();
+        sales.map((item) => {
+          expect(item).to.be.an('object');
+        });
+      });
+
+      it('os itens possuem as propriedades: "saleId", "date", "product_id" e "quantity"', async () => {
+        const sales = await salesModel.getSales();
+        sales.map((item) => {
+          expect(item).to.have.all.keys(["saleId", "date", "product_id", "quantity"]);
+        })
+      });
+    });
+  });
+
+  describe('Ao buscar uma venda pelo id (salesModel.getSaleById)', () => {
+    describe('Se a venda não estiver registrada', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([[]]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('retorna um array', async () => {
+        const sale = await salesModel.getSaleById();
+        expect(sale).to.be.an('array');
+      });
+
+      it('o array está vazio', async () => {
+        const sale = await salesModel.getSaleById();
+        expect(sale).to.be.empty;
+      });
+    });
+
+    describe('Se a venda já estiver registrada', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([saleByIdMock]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('retorna um array', async () => {
+        const sale = await salesModel.getSaleById();
+        expect(sale).to.be.an('array');
+      });
+
+      it('o array não está vazio', async () => {
+        const sale = await salesModel.getSaleById();
+        expect(sale).not.to.be.empty;
+      });
+
+      it('todos os itens do array são do tipo "objeto"', async () => {
+        const sale = await salesModel.getSaleById();
+        sale.map((item) => {
+          expect(item).to.be.an('object');
+        });
+      });
+
+      it('os itens possuem as propriedades: "date", "product_id" e "quantity"', async () => {
+        const sale = await salesModel.getSaleById();
+        sale.map((item) => {
+          expect(item).to.have.all.keys(["date", "product_id", "quantity"]);
+        });
+      });
     });
   });
 });
